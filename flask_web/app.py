8893427e5eed5a1d4641050de617fe790118c139
zowiee <zowiee@gmail.com>
2021-06-30 22:02:30 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return "hello my sweet world!"
+   return "hello my sweet sweet world!"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
