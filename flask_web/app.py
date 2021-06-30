7d8a625c5f1f9cf417d90faf89d0423dd4f04287
zowiee <zowiee@gmail.com>
2021-06-30 19:37:31 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+def home():
+   return "hello world!"
 
 if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+   app.run(debug=True, host='0.0.0.0')
