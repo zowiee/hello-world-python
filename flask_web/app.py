b69047bc22444c80b03bd6f5a5ea6a1c678aef98
zowiee <zowiee@gmail.com>
2021-07-01 20:02:44 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return "hello my sweet sweet world!"
+   return "hello world from Minishift with Docker!"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
