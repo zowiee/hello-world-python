0e89271d0e509adb1613bbf91054967864f18d8a
zowiee <zowiee@gmail.com>
2021-07-02 10:38:39 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -3,10 +3,12 @@ import socket
 
 from flask import Flask
 app = Flask(__name__)
-
+      
 @app.route('/')
-def home():
-   return "Hello world! I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!"
+def home():  
+   f = open("/data/sharedfile.txt")
+   contents = f.read()
+   return "<h1>Hello world!</h1><br/>I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!<br/><br/>Shared file content:<br/><br/>" + contents 
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
