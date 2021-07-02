85de2671db960f8733a02e23cc2d2bb531939db0
zowiee <zowiee@gmail.com>
2021-07-02 10:09:10 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,14 +1,12 @@
 # flask_web/app.py
-import random
+import socket
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   colors = ['\033[95m', '\033[94m', '\033[96m', '\033[92m', '\033[93m', '\033[91m', '\033[0m', '\033[1m', '\033[4m']
-   myColor = random.choice(colors)
-   return myColor + "hello world from MiniShift through Ultrahook!" + myColor
+   return "Hello world! I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
