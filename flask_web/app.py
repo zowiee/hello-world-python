ae239141b95a0d33251f9457bbcb6b49b998aafa
zowiee <zowiee@gmail.com>
2021-07-02 10:59:33 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -11,7 +11,7 @@ def home():
       color = random.choice(colors)
       f = open("/data/sharedfile.txt")
       contents = f.read()
-      return "<div style='background:" + color + "'><h1>Hello world!</h1><br/>I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!<br/><br/>Shared file content:<br/><br/>" + contents + </div>
+      return "<div style='background:" + color + "'><h1>Hello world!</h1><br/>I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!<br/><br/>Shared file content:<br/><br/>" + contents + "</div>"
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
