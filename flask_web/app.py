f3f455a064a88d569531a584ccf74483d41b6f6f
zowiee <zowiee@gmail.com>
2021-07-02 10:51:35 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,14 +1,19 @@
 # flask_web/app.py
 import socket
-
+import random
 from flask import Flask
+
 app = Flask(__name__)
-      
+colors = ['red', 'blue', 'green', 'cyan', 'yellow', 'brown']
+
 @app.route('/')
 def home():  
-   f = open("/data/sharedfile.txt")
-   contents = f.read()
-   return "<h1>Hello world!</h1><br/>I am runnig on host '" + socket.gethostname() + "' from MiniShift through Ultrahook!<br/><br/>Shared file content:<br/><br/>" + contents 
+      color = random.choice(colors)
+      f = open("/data/sharedfile.txt")
+      contents = f.read()
+      return "<div style='background:" + color + "'><h1>Hello world!</h1><br/>I am runnig on host '" + socket.gethostname() 
+            + "' from MiniShift through Ultrahook!<br/><br/>Shared file content:<br/><br/>" + contents 
+            + </div>
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
