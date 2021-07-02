e86f8b60fbd11c29cffeabdf93d0ac5cbe0486cb
zowiee <zowiee@gmail.com>
2021-07-02 10:03:48 +0100

    Update app.py

--- flask_web/app.py
+++ flask_web/app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return "hello world from MiniShift through Ultrahook!"
+   colors = ['\033[95m', '\033[94m', '\033[96m', '\033[92m', '\033[93m', '\033[91m', '\033[0m', '\033[1m', '\033[4m']
+   myColor = random.choice(colors)
+   return myColor + "hello world from MiniShift through Ultrahook!" + myColor
 
 if __name__ == '__main__':
    app.run(debug=True, host='0.0.0.0')
